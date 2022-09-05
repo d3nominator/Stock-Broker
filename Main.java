91360869c071e66df6fdc63a21858ae98233f06d
Rishabh Kumar Pandey <rishabhpandey7681@gmail.com>
2022-09-05 22:55:23 +0530

    Update comment

--- Main.java
+++ Main.java
@@ -13,7 +13,8 @@ public class Main {
        System.out.println(marks[2]);
        System.out.println(marks[3]);
        System.out.println(marks[4]);
-       // System.out.println(marks[5]);
+       System.out.println(marks[5]);
+
        
 
    } 
