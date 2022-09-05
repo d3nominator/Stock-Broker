65fb5e5f4ac1cddec125773184837bc79e4190a4
Rishabh Kumar Pandey <rishabh.pandey7681@gmail.com>
2022-09-05 22:49:27 +0530

    add java file

---
+++ Main.java
@@ -0,0 +1,20 @@
+package Stock-Broker;
+
+public class Main {
+   public static void main(String[] args) {
+       int[] marks  = new int[5];
+       marks[0] = 10;
+       marks[1] = 20;
+       marks[2] = 30;
+       marks[3] = 40;
+       marks[4] = 50;
+       System.out.println(marks[0]);
+       System.out.println(marks[1]);
+       System.out.println(marks[2]);
+       System.out.println(marks[3]);
+       System.out.println(marks[4]);
+       // System.out.println(marks[5]);
+       
+
+   } 
+}
